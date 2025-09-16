7d97a68d68140b0c3dfd2f9c793922750166d922
Snowy-Cuber <tfischerrenee1@gmail.com>
2025-09-16 17:53:18 -0400

    Add project files.

---
+++ Inheritance/Source.cpp
@@ -0,0 +1,25 @@
+// Tamera Fischer
+// CIS 1202
+// 9/16/2025
+
+#include<iostream>
+#include<string>
+#include"Vehicle.h"
+#include"Car.h"
+#include"Truck.h"
+
+using namespace std;
+
+
+int main() {
+
+
+
+
+
+
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
